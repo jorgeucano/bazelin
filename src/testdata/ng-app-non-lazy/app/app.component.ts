40c1ed5444e5a66757242462e1f73e4cdb1ad94d
Volodymyr Kozlov <vovakozlovkh@gmail.com>
2019-03-22 12:00:11 +0200

    WIP: feat(bazelin): remoute build under docker

--- src/testdata/ng-app-non-lazy/app/app.component.ts
+++ src/testdata/ng-app-non-lazy/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-component',
   templateUrl: './app.component.html',
-  styleUrls: ['./some.sass',]
+  // styleUrls: ['./some.sass',]
 })
 export class AppComponent {
 }
